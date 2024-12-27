6064077dd5d1c253a5a48c127f79489d4fee6159
torbenconto <torbenmconto@gmail.com>
2024-12-27 17:28:18 -0500

    Probably non-working print function.

--- printer/printer.go
+++ printer/printer.go
@@ -152,6 +152,11 @@ func (p *Printer) PrintGcodeFile(filePath string) error {
 		}.JSON())
 }
 
+func (p *Printer) Print3mfFile(fileName string, plate int, useAms bool) error {
+	// Probably doesent work. Need to check the correct format of the command
+	return p.MQTTClient.Publish(`{"print": {"command": "project_file", "param": "Metadata/plate_` + string(plate) + `.gcode", "subtask_name": ` + fileName + `, "use_ams": ` + strconv.FormatBool(useAms) + `"bed_leveling": true, "url": "ftp://"` + fileName + `, "bed_type": "auto", "flow_cali": true, "vibration_cali": true, "layer_inspect: true", "ams_mapping": [0]}}`)
+}
+
 // SetBedTemperature sets the bed temperature to a specified number in degrees Celcius using a gcode command.
 func (p *Printer) SetBedTemperature(temperature int) error {
 	return p.MQTTClient.Publish(mqtt.Command{
