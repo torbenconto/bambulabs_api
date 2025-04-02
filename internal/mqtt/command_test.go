0f59ea9cf286b9307103689d865bdf82685cb1d0
torbenconto <torbenmconto@gmail.com>
2025-04-02 13:06:24 -0400

    Add unit test for SetId method in Command struct

--- internal/mqtt/command_test.go
+++ internal/mqtt/command_test.go
@@ -2,6 +2,7 @@ package mqtt
 
 import (
 	"encoding/json"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -23,6 +24,13 @@ func TestCommand_AddParamField(t *testing.T) {
 	cmd := NewCommand(Print)
 	cmd.AddParamField("value")
 	assert.Equal(t, "value", cmd.fields["param"])
+}
+
+func TestCommand_SetId(t *testing.T) {
+	cmd := NewCommand(Print)
+	cmd.SetId(123)
+	assert.Equal(t, "123", cmd.fields["sequence_id"])
+	assert.Equal(t, "123", strconv.Itoa(cmd.id))
 }
 
 func TestCommand_JSON(t *testing.T) {
