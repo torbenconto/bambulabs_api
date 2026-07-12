7c5c7a8f6f16cd7940cb54094541853af3e57ae2
torbenconto <torbenmconto@gmail.com>
2026-07-12 00:01:07 -0400

    remove unused GcodeState enum

--- state.go
+++ state.go
@@ -1,14 +1,1 @@
 package bambulabs_api
-
-// GcodeState is an enum representing the current print state as dictated by printer.
-type GcodeState string
-
-const (
-	IDLE    GcodeState = "IDLE"
-	PREPARE GcodeState = "PREPARE"
-	RUNNING GcodeState = "RUNNING"
-	PAUSE   GcodeState = "PAUSE"
-	FINISH  GcodeState = "FINISH"
-	FAILED  GcodeState = "FAILED"
-	UNKNOWN GcodeState = "UNKNOWN"
-)
