fa8abd0012b0f5a0dfee57e80e8b9f4c18dd59d1
torbenconto <torbenmconto@gmail.com>
2026-09-12 20:28:40 -0400

    feat: additional error messages for future use

--- errors.go
+++ errors.go
@@ -9,5 +9,8 @@ var (
 	ErrLightUnavailable = errors.New("this light is not currently avalible, it may not be supported by your printer")
 	ErrFanUnavailable   = errors.New("this fan is not currently avalible, it may not be supported by your printer")
 
+	ErrInvalidFanPercent = errors.New("fan percent must be between 0 and 100")
+	ErrInvalidLightMode  = errors.New("invalid light mode")
+
 	ErrFTPUnavailable = errors.New("ftp connection unavailable")
 )
